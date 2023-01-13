6af3d1b9e0f99c7890af61e3c477fa52b3140eda
ElbertHo <eho2025@pingry.org>
2023-01-13 15:25:50 -0500

    Update transform_utils.py
    Implemented resample to 500hz and notch filter to remove 50hz and 60hz

--- transform_utils.py
+++ transform_utils.py
@@ -1,2 +1,21 @@
 import torch
 import numpy as np
+from scipy.signal import resample, iirnotch, lfilter
+from sklearn.preprocessing import MinMaxScaler
+
+def resample(ecg_signal, original_freq):
+    new_freq = 500
+    resample(ecg_signal, int(len(ecg_signal) * (new_freq / original_freq)))
+
+def notch_filter(ecg_signal, original_freq):
+    remove_freqs = [50, 60]
+    quality_factor = 20.0
+    for freq in remove_freqs:
+        b,a = iirnotch(freq, quality_factor, original_freq)
+        ecg_signal = lfilter(b, a, ecg_signal)
+def fir():
+    pass
+
+def normalize():
+    pass
+
