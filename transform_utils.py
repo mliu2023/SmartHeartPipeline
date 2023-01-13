3a3227f4c368e5042aca0bc42232d5ae1a9ba1b7
Max Liu <mliu2023@pingry.org>
2023-01-13 15:59:53 -0500

    Update transform_utils.py

--- transform_utils.py
+++ transform_utils.py
@@ -3,16 +3,21 @@ import numpy as np
 from scipy.signal import resample, iirnotch, lfilter
 from sklearn.preprocessing import MinMaxScaler
 
+# resamples the ecg signal to 500 Hz
 def resample(ecg_signal, original_freq):
     new_freq = 500
-    resample(ecg_signal, int(len(ecg_signal) * (new_freq / original_freq)))
+    ecg_signal = resample(ecg_signal, int(len(ecg_signal) * (new_freq / original_freq)))
+    return ecg_signal
 
+# removes specific frequencies from the ecg signal
 def notch_filter(ecg_signal, original_freq):
     remove_freqs = [50, 60]
     quality_factor = 20.0
     for freq in remove_freqs:
         b,a = iirnotch(freq, quality_factor, original_freq)
         ecg_signal = lfilter(b, a, ecg_signal)
+    return ecg_signal
+
 def fir():
     pass
 
