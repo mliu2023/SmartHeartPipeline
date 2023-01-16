3ff737fda7a3df623743e79d74e594eff2fecee4
Max Liu <mliu2023@pingry.org>
2023-01-16 15:50:26 -0500

    Added training and evaluation.
    Added a warmup learning rate scheduler, training, evaluation, and some missing evaluation files.  Evaluation should be able to handle a batch size greater than 1.  Still needs to be tested.

--- train_12ECG_classifier.py
+++ train_12ECG_classifier.py
@@ -5,14 +5,16 @@ from torch.utils.data import DataLoader
 import numpy as np
 import sklearn
 import os
-from get_12ECG_features import get_12ECG_features
+import tqdm
 from dataset import ECGDataset
 from transform_utils import *
 
 def train_12ECG_classifier(input_directory, output_directory):
 
     # initializing class labels
-    classes = [270492004, 164889003, 164890007, 426627000, 713427006, 713426002, 445118002, 39732003, 164909002, 251146004, 698252002, 10370003, 284470004, 427172004, 164947007, 111975006, 164917005, 47665007, 59118001, 427393009, 426177001, 426783006, 427084000, 63593006, 164934002, 59931005, 17338001]
+    classes = [270492004, 164889003, 164890007, 426627000, 713427006, 713426002, 445118002, 39732003, 164909002, 
+               251146004, 698252002, 10370003, 284470004, 427172004, 164947007, 111975006, 164917005, 47665007, 
+               59118001, 427393009, 426177001, 426783006, 427084000, 63593006, 164934002, 59931005, 17338001]
     for i in range(len(classes)):
         classes[i] = str(classes[i])
 
@@ -25,10 +27,12 @@ def train_12ECG_classifier(input_directory, output_directory):
             filenames.append(filepath)
         if not file.lower().startswith('.') and file.lower().endswith('hea') and os.path.isfile(filepath):
             labels.append(get_labels_from_header(file))
+    filenames = np.array(filenames)
+    labels = np.array(labels)
 
     # composing preprocessing methods
-    train_transforms = Compose([resample, normalize, notch_filter])
-    val_transforms = Compose([resample, normalize, notch_filter])
+    train_transforms = Compose([Resample, Normalize, Notch_filter])
+    val_transforms = Compose([Resample, Normalize, Notch_filter])
 
     # train model
     stratified_k_fold = sklearn.model_selection.StratifiedKFold(n_splits=10)
@@ -44,19 +48,81 @@ def train_12ECG_classifier(input_directory, output_directory):
         # define the model
         model = CoolResNet18ThatWeFoundOnTheInternet()
 
-        # define training parameters and functions
+        # define training parameters, loss, optimizer, and learning rate scheduler
         warmup_epochs = 10
-        epochs = 100
+        total_epochs = 100
+        lr = 1e-3
         loss = torch.nn.BCELoss()
-        optimizer = torch.optim.Adam(model.parameters, lr=1e-3, weight_decay=1e-5)
-        lr_scheduler = Warmup_LR_Scheduler(optimizer)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-5)
+        cosine_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, total_epochs-warmup_epochs)
+        lr_scheduler = Warmup_LR(optimizer=optimizer,
+                                warmup_iteration=warmup_epochs,
+                                target_lr=lr,
+                                after_scheduler=cosine_scheduler)
 
-        for i in range(epochs):
-            print(f'Epoch {i+1}/{epochs}' + '\n')
-            train(model, train_loader, loss, optimizer, lr_scheduler=None)
-            val(model, val_loader)
+        # training + validation loop
+        print(f'Training on fold {i+1}')
+        for epoch in range(1, total_epochs+1):
+            print(f'Epoch {epoch}/{total_epochs}')
+            train(model, train_loader, loss, optimizer, lr_scheduler)
+            val(model, val_loader, classes, filenames[val_indices], output_directory)
+
+# performs one training iteration
+def train(model, train_loader, loss, optimizer, lr_scheduler=None):
+    with tqdm(train_loader, unit='batch') as minibatch:
+        for x,y in minibatch:
+            # .cuda() loads the data onto the GPU
+            # this code will not run without a compatible NVIDIA GPU
+            x = x.cuda()
+            y = y.cuda()
+
+            y_pred = model(x)
+            loss = loss(y_pred, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            minibatch.set_postfix(loss=loss.item())
+    if(lr_scheduler is not None):
+        lr_scheduler.step()
+
+# performs one validation iteration and outputs the challenge score
+def val(model, val_loader, classes, filenames, output_directory):
+    with tqdm(val_loader, unit='batch') as minibatch:
+        for x,y in minibatch:
+            # .cuda() loads the data onto the GPU
+            # this code will not run without a compatible NVIDIA GPU
+            x = x.cuda()
+            y = y.cuda()
+
+            y_pred = model(x)
+
+            # evaluating the challenge score
+            y_pred = torch.sigmoid(y_pred).squeeze() # sigmoid activation for multilabel classification
+            y_label_pred = y_pred > 0.5 # the threshold will be adjusted after the model is done training
+
+            y_pred = y_pred.detach().cpu().numpy()
+            y_label_pred = np.array(y_label_pred.detach().cpu().numpy(), dtype=np.uint8)
 
 
+            save_predictions(output_directory,filenames,scores=y_pred,labels=y_label_pred,classes=classes)
+
+def save_predictions(output_directory,filenames,scores,labels,classes):
+    for filename in filenames:
+        recording = os.path.splitext(filename)[0]
+        new_file = filename.replace('.mat','.csv')
+        output_file = os.path.join(output_directory,new_file)
+
+        # Include the filename as the recording number
+        recording_string = '#{}'.format(recording)
+        class_string = ','.join(classes)
+        label_string = ','.join(str(i) for i in labels)
+        score_string = ','.join(str(i) for i in scores)
+
+        with open(output_file, 'w') as f:
+            f.write(recording_string + '\n' + class_string + '\n' + label_string + '\n' + score_string + '\n')
+
+# gets the class labels from the given header file(s)
 def get_labels_from_header(header_file, classes):
     with open(header_file, 'r') as file:
         lines = file.readlines()
@@ -69,3 +135,26 @@ def get_labels_from_header(header_file, classes):
                 class_index = classes.index(diagnosis)
                 labels[class_index] = 1
         return labels
+
+# warmup learning rate scheduler
+class Warmup_LR(object):
+    def __init__(self, optimizer, warmup_iteration, target_lr, after_scheduler=None):
+        self.optimizer = optimizer
+        self.warmup_iteration = warmup_iteration
+        self.target_lr = target_lr
+        self.after_scheduler = after_scheduler
+        self.step(1)
+
+    def warmup_learning_rate(self, current_iteration):
+        warmup_lr = self.target_lr*float(current_iteration)/float(self.warmup_iteration)
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = warmup_lr
+
+    def step(self, current_iteration):
+        if current_iteration <= self.warmup_iteration:
+            self.warmup_learning_rate(current_iteration)
+        else:
+            self.after_scheduler.step(current_iteration-self.warmup_iteration)
+    
+    def load_state_dict(self, state_dict):
+        self.after_scheduler.load_state_dict(state_dict)
