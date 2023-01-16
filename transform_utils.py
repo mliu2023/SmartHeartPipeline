4323e69f597c4ffc8aebff27ba0c1a2c7aaf9946
Max Liu <mliu2023@pingry.org>
2023-01-15 23:04:19 -0500

    Update transform_utils.py
    Made all the transforms into classes so we can compose them together.  Added Normalize class.

--- transform_utils.py
+++ transform_utils.py
@@ -1,26 +1,59 @@
-import torch
 import numpy as np
 from scipy.signal import resample, iirnotch, lfilter
-from sklearn.preprocessing import MinMaxScaler
+
+# composes a list of transforms together
+# all transforms should take in an ndarray and return an ndarray
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, ecg_signal, source_freq):
+        for t in self.transforms:
+            # if the transform is resample, then we have to pass in the source frequency as well
+            if(isinstance(t, Resample)):
+                ecg_signal = t(ecg_signal, source_freq)
+            else:
+                ecg_signal = t(ecg_signal)
+        return ecg_signal
 
 # resamples the ecg signal to 500 Hz
-def resample(ecg_signal, original_freq):
-    new_freq = 500
-    ecg_signal = resample(ecg_signal, int(len(ecg_signal) * (new_freq / original_freq)))
-    return ecg_signal
+class Resample(object):
+    def __init__(self, new_freq = 500):
+        self.new_freq = new_freq
+    def __call__(self, ecg_signal, source_freq = 500):
+        ecg_signal = resample(ecg_signal, int(len(ecg_signal) * (self.new_freq / source_freq)), axis=1)
+        return ecg_signal
 
 # removes specific frequencies from the ecg signal
-def notch_filter(ecg_signal, original_freq):
-    remove_freqs = [50, 60]
-    quality_factor = 20.0
-    for freq in remove_freqs:
-        b,a = iirnotch(freq, quality_factor, original_freq)
-        ecg_signal = lfilter(b, a, ecg_signal)
-    return ecg_signal
+class Notch_filter(object):
+    def __init__(self, remove_freqs = [50, 60]):
+        self.remove_freqs = remove_freqs
+    def __call__(self, ecg_signal, source_freq = 500):
+        quality_factor = 20.0
+        for freq in self.remove_freqs:
+            b,a = iirnotch(freq, quality_factor, source_freq)
+            ecg_signal = lfilter(b, a, ecg_signal)
+        return ecg_signal
 
-def fir():
+class FiniteImpulseResponse():
     pass
 
-def normalize():
-    pass
+class Normalize(object):
+    def __init__(self, type="min-max"):
+        self.type = type
+
+    def __call__(self, ecg_signal):
+        if self.type == "min-max":
+            max = np.amax(ecg_signal, axis=1)
+            min = np.amin(ecg_signal, axis=1)
+            ecg_signal = (ecg_signal-min)/(max-min)
+        elif self.type == "mean-std":
+            mean = np.mean(ecg_signal, axis=1)
+            std = np.std(ecg_signal, axis=1)
+            ecg_signal = (ecg_signal-mean)/std
+        elif self.type == "none":
+            ecg_signal = ecg_signal
+        else:
+            raise NameError(f'Normalization type {self.type} is not included.')
+        return ecg_signal
 
