9988f226a4c94bd278612ee48973e667d1e0eab7
DaGr8Nave <evan.h.wen@gmail.com>
2023-01-23 09:03:51 -0500

    added code for an old mode;
    added code from last year model
    evan

--- config2.py
+++ config2.py
@@ -1,8 +1,9 @@
 import torch
 from warmup_lr import GradualWarmupScheduler
 from models.SEResNet import ResNet
+from models.ResNet1dTransformer import ResNet1D
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = ResNet(kernel_size=7).to(device)
+model = ResNet1D(n_block = 12, base_filters = 32, kernel_size = 14, stride = 3, in_channels = 12, groups=1, n_classes=27).cuda()
 warmup_epochs = 10
 total_epochs = 100
 min_length = 7500
